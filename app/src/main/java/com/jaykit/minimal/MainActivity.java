cc4710047996e9ef6e06f0d3e67f9ca01f369311
Kiet Duong Hung <57902884+jaykitpro@users.noreply.github.com>
2021-05-18 17:56:50 +0700

    Clean MainActivity.java

--- app/src/main/java/com/jaykit/minimal/MainActivity.java
+++ app/src/main/java/com/jaykit/minimal/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -17,7 +16,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import org.jetbrains.annotations.NotNull;
@@ -31,7 +29,7 @@ import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Object> permissionToRequest;
-    private final ArrayList permissionRejected = new ArrayList();
+    private final ArrayList<String> permissionRejected = new ArrayList<>();
     private final ArrayList<String> permissions = new ArrayList<>();
 
     private final static int ALL_PERMISSIONS_RESULT = 101;
@@ -57,9 +55,6 @@ public class MainActivity extends AppCompatActivity {
             BottomNavigationView navView = findViewById(R.id.nav_view);
             // Passing each menu ID as a set of Ids because each
             // menu should be considered as top level destinations.
-            AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_user)
-                    .build();
             NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
             //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
             NavigationUI.setupWithNavController(navView, navController);
@@ -70,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         permissionToRequest = findUnAskedPermissions(permissions);
 
         if ( permissionToRequest.size() > 0 ) {
-            requestPermissions((String[]) permissionToRequest.toArray(new String[0]), ALL_PERMISSIONS_RESULT);
+            requestPermissions(permissionToRequest.toArray(new String[0]), ALL_PERMISSIONS_RESULT);
         }
         gps = new GPSTracker(this);
 
@@ -106,25 +101,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case ALL_PERMISSIONS_RESULT:
-                for (Object perms : permissionToRequest) {
-                    if (hasPermission((String) perms))
-                        permissionRejected.add(perms);
+        if (requestCode == ALL_PERMISSIONS_RESULT) {
+            for (Object perms : permissionToRequest) {
+                if (hasPermission((String) perms))
+                    permissionRejected.add((String) perms);
+            }
+            if (permissionRejected.size() > 0) {
+                if (shouldShowRequestPermissionRationale(permissionRejected.get(0))) {
+                    showMessageOKCancel(
+                            (dialog, which) -> requestPermissions(permissionRejected.toArray(new String[0]), ALL_PERMISSIONS_RESULT));
                 }
-                if ( permissionRejected.size() > 0 ) {
-                    if (shouldShowRequestPermissionRationale((String) permissionRejected.get(0))) {
-                        showMessageOKCancel(
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        requestPermissions((String[]) permissionRejected.toArray(new String[0]), ALL_PERMISSIONS_RESULT);
-                                    }
-                                });
-                        return;
-                    }
-                    break;
-                }
+            }
         }
     }
     private void showMessageOKCancel(DialogInterface.OnClickListener okListener) {
